faba4c3e51850d59519ceabddfe7d6852df23b77
trekkabout <robertstephens225@hotmail.com>
2020-06-24 14:00:11 -0500

    reddit app

---
+++ src/app/models/article.model.ts
@@ -0,0 +1,30 @@
+
+export class Article {
+  votes: number;
+  title: string;
+  link: string;
+
+  constructor(title: string, link: string, votes?: number) {
+    this.title = title;
+    this.link = link;
+    this.votes = votes || 0;
+  }
+
+  voteUp(): void {
+    this.votes += 1;
+  }
+
+  voteDown(): void {
+    this.votes -= 1;
+  }
+
+  domain(): string {
+    try {
+      const domainAndPath: string = this.link.split('//')[1];
+      return domainAndPath.split('/')[0];
+    } catch (err) {
+      return null;
+    }
+  }
+
+}
